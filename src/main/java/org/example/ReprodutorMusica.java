19d37d69cb57c2784fc97857e714525bb98487af
danielssaab <104411350+danielssaab@users.noreply.github.com>
2024-03-19 22:43:25 -0300

    teste2

--- src/main/java/org/example/ReprodutorMusica.java
+++ src/main/java/org/example/ReprodutorMusica.java
@@ -1,2 +1,29 @@
-package org.example;public class ReprodutorMusica {
+package org.example;
+
+public class ReprodutorMusica {
+
+	private ReprodutorMusica() {}
+	private static ReprodutorMusica instance = new ReprodutorMusica();
+	public static ReprodutorMusica obterInstancia() {
+		return instance;
+	}
+
+	private int volume;
+	private boolean modoAleatorio;
+
+	public int obterVolume() {
+		return volume;
+	}
+
+	public void definirVolume(int volume) {
+		this.volume = volume;
+	}
+
+	public boolean estaNoModoAleatorio() {
+		return modoAleatorio;
+	}
+
+	public void definirModoAleatorio(boolean modoAleatorio) {
+		this.modoAleatorio = modoAleatorio;
+	}
 }
--- src/test/java/org/example/ReprodutorMusicaTest.java
+++ src/test/java/org/example/ReprodutorMusicaTest.java
@@ -1,6 +1,19 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 class ReprodutorMusicaTest {
-  
+
+	@Test
+	public void deveRetornarVolume() {
+		ReprodutorMusica.obterInstancia().definirVolume(50);
+		assertEquals(50, ReprodutorMusica.obterInstancia().obterVolume());
+	}
+
+	@Test
+	public void deveRetornarModoAleatorio() {
+		ReprodutorMusica.obterInstancia().definirModoAleatorio(true);
+		assertEquals(true, ReprodutorMusica.obterInstancia().estaNoModoAleatorio());
+	}
 }
